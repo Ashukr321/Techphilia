8141d0ae67ca8ce67dffb2554f4badd8c22be049
Ashukr321 <coderashukr321@gmail.com>
2024-01-08 23:21:53 +0530

    fix bug

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -32,26 +32,6 @@ const message =()=>{
   var msg = document.getElementById('msg');
   const success = document.getElementById('success');
   
-
-  if(Name.value === '' || email.value === '' || msg.value === ''){
-      danger.style.display = 'block';
-  }
-  else{
-      setTimeout(() => {
-          Name.value = '';
-          email.value = '';
-          msg.value = '';
-      }, 2000);
-
-      success.style.display = 'block';
-  }
-
-
-  setTimeout(() => {
-      danger.style.display = 'none';
-      success.style.display = 'none';
-  }, 4000);
-
 }
   return (
     <section class="bg-white dark:bg-gray-900 h-[555px] mt-10 mb-8 max-[768px]:m-auto max-[768px]:mb-4 rounded-2xl max-[900px]:mr-2">
