207dcc558ab7c59c91891e418d8893ad49f1cbcc
Ashukr321 <coderashukr321@gmail.com>
2024-01-09 11:52:47 +0530

    fix bug

--- src/Data/TechTeam.js
+++ src/Data/TechTeam.js
@@ -1,7 +1,21 @@
 import ashutosh from '../assets/tech_team/ashutosh.jpg';
 import angel  from  '../assets/tech_team/angel.png'
 const Developer = [
-   
+    {
+        name:"Anant Kumar",
+        img:" https://firebasestorage.googleapis.com/v0/b/bookleyt.appspot.com/o/WhatsApp%20Image%202023-03-12%20at%2019.20.45.jpg?alt=media&token=8e7e911a-ddbf-43c1-a21e-694cb5a97d1b",
+        role:"Developer",
+        profile:"https://www.linkedin.com/in/anant-kumar-b740a9254/",
+        about:"Amity University Patna",
+    },
+    {
+        name:"Aman Kumar Pandey",
+        img:"https://lh3.googleusercontent.com/pw/AMWts8C7nqWgOFXN5q6L7SMmTbgyqRM6JrpR7IXaURvJAPu2dqRIXKY2anr2DXZGi5X2DD6wUsOMg9gRIk3MhUdFrPFRI63lR9ctf94Uf7boyFGyOcOOzKk=w2400",
+        role:"Developer",
+        profile:"https://www.linkedin.com/in/amanpandey2101/",
+        about:"Amity University Patna",
+       
+    },
    
     {
         name:"Nikhil Ranjan",
